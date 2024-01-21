30d79d80053fea1afeeaac1fcf941083f2d460ef
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-21 14:27:06 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GA_TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            try
+            {
+                // try / catch
+                Console.Write("Please enter a whole number: ");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+
+            } // End of try
+            catch
+            {
+                Console.WriteLine("The format was invalid");
+            }
+
+            // try / catch
+
+            Console.WriteLine(number);
+
+        } // Main
+
+    } // void
+
+} // class
